bebdf76ee5ae32a465940a74c408e3a0211c0f2b
JoujonikiAsa2 <nibeditaroy582@gmail.com>
2024-12-18 23:10:53 +0600

    install all dependencies and setup eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+// eslint.config.mjs
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.browser } },
+  {
+    rules: {
+      "@typescript-eslint/no-require-imports": "error",
+      "no-unused-vars": "error",
+      "no-unused-expressions": "error",
+      "prefer-const": "error",
+      "no-console": "warn",
+      "no-undef": "error",
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: [".node_modules/*"],
+  },
+];
