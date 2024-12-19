bd76bd662eb82dc47f3ede32896bfa1ba1e90d77
JoujonikiAsa2 <nibeditaroy582@gmail.com>
2024-12-19 16:06:13 +0600

    update dist code

--- dist/app/modules/blog/blog.model.js
+++ dist/app/modules/blog/blog.model.js
@@ -52,6 +52,12 @@ const blogSchema = new mongoose_1.default.Schema({
         type: Boolean,
         default: true,
     },
+    createdAt: {
+        type: Date,
+    },
+    updatedAt: {
+        type: Date,
+    },
 }, {
     timestamps: true,
 });
