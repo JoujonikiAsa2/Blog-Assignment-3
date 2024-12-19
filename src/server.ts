4ba92242e73a0259d24ca93d4d9c5e7d7389dae2
JoujonikiAsa2 <nibeditaroy582@gmail.com>
2024-12-19 09:39:47 +0600

    add mongodb connection and handle uncaughtexeption and unhandledrejection

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,32 @@
-import app from "./app";
-import config from "./app/config";
-const PORT = config.port || 5000 
+import mongoose from 'mongoose'
+import app from './app'
+import config from './app/config'
+import { Server } from 'http'
+const PORT = config.port || 5000
+let server: Server
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`)
+const main = async () => {
+  try {
+    await mongoose.connect(config.mongo_db_url as string)
+    server = app.listen(PORT, () => {
+      console.log(`Blog server is listening on port ${PORT}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+main()
+
+process.on('unhandledRejection', () => {
+  console.log('Server closed due to unhandledRejection')
+  server.close(() => {
+    process.exit(1)
+  })
+  process.exit(1)
+})
+
+process.on('uncaughtException', () => {
+  console.log('Server closed due to uncaughtException')
+  process.exit(1)
 })
