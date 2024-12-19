7ac1563e2610280439c5c3a6227735102562edd8
JoujonikiAsa2 <nibeditaroy582@gmail.com>
2024-12-19 09:41:10 +0600

    update dist

--- dist/server.js
+++ dist/server.js
@@ -1,10 +1,42 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
 const app_1 = __importDefault(require("./app"));
-const PORT = 5000;
-app_1.default.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+const config_1 = __importDefault(require("./app/config"));
+const PORT = config_1.default.port || 5000;
+let server;
+const main = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield mongoose_1.default.connect(config_1.default.mongo_db_url);
+        server = app_1.default.listen(PORT, () => {
+            console.log(`Blog server is listening on port ${PORT}`);
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
+main();
+process.on('unhandledRejection', () => {
+    console.log('Server closed due to unhandledRejection');
+    server.close(() => {
+        process.exit(1);
+    });
+    process.exit(1);
+});
+process.on('uncaughtException', () => {
+    console.log('Server closed due to uncaughtException');
+    process.exit(1);
 });
