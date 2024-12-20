4a4cb502ec067330b47b984005559fc98f9d218b
JoujonikiAsa2 <nibeditaroy582@gmail.com>
2024-12-20 21:09:14 +0600

    update home route

--- dist/app.js
+++ dist/app.js
@@ -6,19 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-const http_status_1 = __importDefault(require("http-status"));
 const globalErrorHandler_1 = __importDefault(require("./app/middlewares/globalErrorHandler"));
 const routes_1 = __importDefault(require("./app/routes"));
 const notFoundError_1 = __importDefault(require("./app/middlewares/notFoundError"));
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
+app.use(express_1.default.static("public"));
 app.use('/api', routes_1.default);
-app.get('/', (req, res) => {
-    res.status(http_status_1.default.OK).json({
-        status: http_status_1.default.OK,
-        message: 'Server is running successfully'
-    });
-});
 app.use(globalErrorHandler_1.default);
 app.use(notFoundError_1.default);
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,15 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application } from 'express'
 import cors from 'cors'
 const app:Application = express()
-import httpStatus from 'http-status'
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
 import router from './app/routes'
 import notFoundError from './app/middlewares/notFoundError'
 
 app.use(express.json())
 app.use(cors())
-app.use('/api', router)
+app.use(express.static("public"))
 
-app.get('/', (req:Request, res: Response) => {
-    res.status(httpStatus.OK).json({
-        status: httpStatus.OK,
-        message: 'Server is running successfully'
-    })
-})
+app.use('/api', router)
 
 app.use(globalErrorHandler)
 app.use(notFoundError)
